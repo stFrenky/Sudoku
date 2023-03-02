418db7d9a8f8d40f1e32721d4e657dfbe339ffcc
Shamil <shoma.albakov69@gmail.com>
2023-03-02 07:02:16 +0300

    add feach

--- generation.js
+++ generation.js
@@ -1,5 +1,6 @@
 const view = {
     field: document.querySelector('.field'),
+    cellLIst: document.querySelectorAll('.cell'),
     digits: document.querySelector('.btn-list'),
     selectedCell: null,
 
@@ -30,39 +31,79 @@ const view = {
 
 view.init();
 
-// const canvas = {
-//     row: {
-//         1: new Set(),
-//         2: new Set(),
-//         3: new Set(),
-//         4: new Set(),
-//         5: new Set(),
-//         6: new Set(),
-//         7: new Set(),
-//         8: new Set(),
-//         9: new Set(),
-//     },
-//     c: {
-//         1: new Set(),
-//         2: new Set(),
-//         3: new Set(),
-//         4: new Set(),
-//         5: new Set(),
-//         6: new Set(),
-//         7: new Set(),
-//         8: new Set(),
-//         9: new Set(),
-//     },
-//     b: {
-//         1: new Set(),
-//         2: new Set(),
-//         3: new Set(),
-//         4: new Set(),
-//         5: new Set(),
-//         6: new Set(),
-//         7: new Set(),
-//         8: new Set(),
-//         9: new Set(),
-//     },
-// }
+const generation = {
+    sudokuField: [
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+    ],
+
+    rows: [
+        new Set(), new Set(), new Set(),
+        new Set(), new Set(), new Set(),
+        new Set(), new Set(), new Set(),
+    ],
+
+    columns: {
+        1: new Set(),
+        2: new Set(),
+        3: new Set(),
+        4: new Set(),
+        5: new Set(),
+        6: new Set(),
+        7: new Set(),
+        8: new Set(),
+        9: new Set(),
+    },
+
+    blocks: {
+        1: new Set(),
+        2: new Set(),
+        3: new Set(),
+        4: new Set(),
+        5: new Set(),
+        6: new Set(),
+        7: new Set(),
+        8: new Set(),
+        9: new Set(),
+    },
+
+    randomazier() {
+        return (Math.random() * (9 - 1) + 1).toFixed()
+    },
+
+    __proto__: view,
+}
+
+
+
+        var field = '0681594327597283416342671589934157268278936145156842973729318654813465792465729831';
+        var arr = Array(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        for (
+    var j = 0;
+    j
+        <
+        9;j++
+    ) arr.push(arr.splice((Math.random() * arr.length
+    ), 1
+    ))
+    ;
+        for (
+    var i = 1;
+    i
+        <
+        82;
+    i++
+    ) {
+        if (i%9==1) document.write('<tr>');
+        document.write( (Math.random()*10>5) ? '<td>'+arr[field.substr(i,1)-1] + '</td>' : '<td class="l"><input type="text" size="1" maxlength="1" onkeydown = "javascript: return ((event.keyCode>47)&&(event.keyCode<58))"></td>' );
+        if (i%9==0) document.write('</tr>');
+    }
+
 
