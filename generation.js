1d34f59ed34a9103c0a1e9aba4b668bfdc159c77
Shamil <shoma.albakov69@gmail.com>
2023-03-21 21:10:43 +0300

    update generation

--- generation.js
+++ generation.js
@@ -16,6 +16,7 @@ const view = {
 
 		this.selectedCell = target;
 		this.selectedCell.classList.add('active');
+
 	},
 
 	changeValueField(event) {
@@ -38,8 +39,8 @@ const generation = {
 	arr: [1, 2, 3, 4, 5, 6, 7, 8, 9],
 	rows: [],
 	row: null,
-	currPos: null,
-
+	boxRow: null,
+	boxCol: null,
 
 	randomizer() {
 		return Math.random() * this.arr.length;
@@ -55,12 +56,17 @@ const generation = {
 		}
 
 		for (let i = 1; i < 82; i++) {
+			// this.cellLIst[i - 1].style.background = 'white';
+
 			if (i % 9 === 1) this.row = [];
 
 			if (Math.random() * 10 > 5) {
-				this.row.push(this.cellLIst[i - 1].textContent = this.arr[this.pattern.substr(i, 1) - 1]);
+				this.cellLIst[i - 1].textContent = this.arr[this.pattern.substr(i, 1) - 1];
+				this.row.push(this.cellLIst[i - 1]);
+
 			} else {
-				this.row.push(this.cellLIst[i - 1].textContent = '');
+				this.cellLIst[i - 1].textContent = '';
+				this.row.push(this.cellLIst[i - 1]);
 			}
 
 			if (this.cellLIst[i - 1].textContent !== '') {
@@ -74,53 +80,55 @@ const generation = {
 				this.row = null;
 			}
 		}
-
 	},
 
 	findEmpty() {
 		for (let r = 0; r < 9; r++) {
 			for (let c = 0; c < 9; c++) {
-				if (this.rows[r][c].classList === 'action') {
-					this.currPos = [r, c];
+				if (this.rows[r][c].classList[1] === 'active') {
+					return [r,c]
 				}
-
-				this.currPos = null;
 			}
 		}
 	},
 
-	highlightField(a,b) {
-		this.rows[a][b].style.background = 'red';
+	highlightField: {
+		marked(a,b) {
+			this.rows[a][b].classList.add('marked');
+		},
+		unmarked(a, b){
+			this.rows[a][b].classList.remove('marked');
+		}
 	},
 
 	validField() {
-		const [r,c] = this.currPos;
+		const [r,c] = this.findEmpty();
 
 		//rows
 		for (let i = 0; i < 9; i++) {
-			if (this.rows[i][c] === this.rows[r][c] && i !== r) {
-				this.highlightField(i,c);
-				this.highlightField(r,c);
+			if (this.rows[i][c].textContent === this.rows[r][c].textContent && i !== r) {
+				this.highlightField.marked.bind(this)(i,c);
+				this.highlightField.marked.bind(this)(r,c);
 			}
 		}
 
 		//columns
 		for (let i = 0; i < 9; i++) {
-			if (this.rows[r][i] === this.rows[r][c] && i !== c) {
-				this.highlightField(r,i);
-				this.highlightField(r,c);
+			if (this.rows[r][i].textContent === this.rows[r][c].textContent && i !== c) {
+				this.highlightField.marked.bind(this)(r,i);
+				this.highlightField.marked.bind(this)(r,c);
 			}
 		}
 
 		//box
-		this.boxRow = Math.floor(r / 9) * 9;
-		this.boxCol = Math.floor(c / 9) * 9;
+		this.boxRow = Math.floor(r / 3) * 3;
+		this.boxCol = Math.floor(c / 3) * 3;
 
-		for (let i = 0; i < this.boxRow + 9; i++) {
-			for (let j = this.boxCol; j < this.boxCol + 9; j++) {
-				if (this.rows[i][j] === this.rows[r][c] && i !== r && j !== c) {
-					this.highlightField(i,j);
-					this.highlightField(r,c);
+		for (let i = this.boxRow; i < this.boxRow + 3; i++) {
+			for (let j = this.boxCol; j < this.boxCol + 3; j++) {
+				if (this.rows[i][j].textContent === this.rows[r][c].textContent && i !== r && j !== c) {
+					this.highlightField.marked.bind(this)(i,j);
+					this.highlightField.marked.bind(this)(r,c);
 				}
 			}
 		}
